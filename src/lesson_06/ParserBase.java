caee064e900f838b23616720f342c7455e462de7
Peter Thiemann <peter.thiemann@gmail.com>
2015-04-29 12:11:01 +0200

    comments added

--- src/lesson_06/ParserBase.java
+++ src/lesson_06/ParserBase.java
@@ -5,26 +5,37 @@ import java.util.regex.Pattern;
 
 public class ParserBase {
 
+	/**
+	 * input buffer. invariant: never starts with whitespace
+	 */
 	private String inp;
 
 	public ParserBase(String inp) {
 		this.inp = inp.trim();
 	}
 
-	public boolean hasNext(String pattern) {
-		Matcher m = Pattern.compile(pattern).matcher(inp);
+	/**
+	 * Checks the next lexeme in the input. Consumes no input.
+	 * @param regexp a string that specifies the syntax of the lexeme
+	 * @return true if next lexeme in input matches regexp
+	 */
+	public boolean hasNext(String regexp) {
+		Matcher m = Pattern.compile(regexp).matcher(inp);
 		return m.lookingAt();
 	}
 
-	protected String nextToken(String regex) {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher m = pattern.matcher(inp);
+	/**
+	 * Consume the next lexeme of the input if it matches regexp.
+	 * @param regexp a string that specifies the syntax of the lexeme
+	 * @return the next lexeme if it matches regexp. Null, otherwise.
+	 */
+	protected String nextToken(String regexp) {
+		Matcher m = Pattern.compile(regexp).matcher(inp);
 		String r = null;
 		if (m.lookingAt()) {
 			r = inp.substring(0, m.end());
 			inp = inp.substring(m.end()).trim();
 		}
-		// System.out.println("nextToken(\"" + pattern + "\") returns: " + r);
 		return r;
 	}
 
