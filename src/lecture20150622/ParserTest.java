012c4b811572bb67581b90612c2d10303f685523
Peter Thiemann <peter.thiemann@gmail.com>
2015-06-22 21:06:05 +0200

    further tests

--- src/lecture20150622/ParserTest.java
+++ src/lecture20150622/ParserTest.java
@@ -27,17 +27,78 @@ public class ParserTest {
 
 	@Test
 	public void testParseExpr() {
-		fail("Not yet implemented");
+		String input = "42";
+		IExpr expected = cnst(42);
+		checkParseExpr(input, expected);
+		
+		input = "42 + x";
+		expected = add(cnst(42), var("x"));
+		checkParseExpr(input, expected);
+		
+		input = "4711 * z";
+		expected = product(cnst(4711), var("z"));
+		checkParseExpr(input, expected);
+		
+		input = "1 + 2 * 3";
+		expected = add(cnst(1), product(cnst(2), cnst(3)));
+		checkParseExpr(input, expected);
+		
+		input = "(1 + 2) * 3";
+		expected = product(add(cnst(1), cnst(2)), cnst(3));
+		checkParseExpr(input, expected);
+	}
+
+	private void checkParseExpr(String input, IExpr expected) {
+		IScanner scan = new StringScanner(input);
+		IExpr result = new Parser(scan).parseExpr();
+		assertEquals(expected, result);
 	}
 
 	@Test
 	public void testParseExpr1() {
-		fail("Not yet implemented");
+		String input = "";
+		IExpr stop = var("stop");
+		IExpr expected = stop;
+		checkParseExpr1(input, stop, expected);
+		
+		input = "+ xy";
+		expected = add(stop, var("xy"));
+		checkParseExpr1(input, stop, expected);
+		
+		input = "+ x * y";
+		expected = add(stop, product(var("x"), var("y")));
+		checkParseExpr1(input, stop, expected);
+		
+		input = "+ x*y + 13";
+		expected = add (add(stop, product(var("x"), var("y"))), cnst(13));
+		checkParseExpr1(input, stop, expected);
+	}
+
+	private void checkParseExpr1(String input, IExpr stop, IExpr expected) {
+		IScanner scan = new StringScanner(input);
+		IExpr result = new Parser(scan).parseExpr1(stop);
+		assertEquals(expected, result);
 	}
 
 	@Test
 	public void testParseTerm() {
-		fail("Not yet implemented");
+		String input = "level";
+		IExpr expected = var("level");
+		checkParseTerm(expected, input);
+		
+		input = "level * 17";
+		expected = product(var("level"), cnst(17));
+		checkParseTerm(expected, input);
+		
+		input = "2 * xx * yy";
+		expected = product(product(cnst(2), var("xx")), var("yy"));
+		checkParseTerm(expected, input);
+	}
+
+	private void checkParseTerm(IExpr expected, String input) {
+		IScanner scan = new StringScanner(input);
+		IExpr result = new Parser(scan).parseTerm();
+		assertEquals(expected, result);
 	}
 
 	@Test
@@ -45,6 +106,18 @@ public class ParserTest {
 		String input = "";
 		IExpr stop = var("stop");
 		IExpr expected = stop;
+		checkTestTerm1(input, stop, expected);
+		
+		input = "* index";
+		expected = product(stop, var("index"));
+		checkTestTerm1(input, stop, expected);
+		
+		input = "   *42 ";
+		expected = product(stop, cnst(42));
+		checkTestTerm1(input, stop, expected);
+	}
+
+	private void checkTestTerm1(String input, IExpr stop, IExpr expected) {
 		IScanner scan = new StringScanner(input);
 		IExpr result = new Parser(scan).parseTerm1(stop);
 		assertEquals(expected, result);
